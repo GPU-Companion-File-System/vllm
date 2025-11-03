4ddfaf704e865b0a7e8a294ec4c69d7355196181
wangxintao2002 <2454381779@qq.com>
2025-11-03 06:16:36 +0000

    offline example

--- examples/offline_inference/basic/basic.py
+++ examples/offline_inference/basic/basic.py
@@ -13,15 +13,9 @@ import torch
 
 import os
 
-# export TARDIS_CONFIG_FILE="/home/wxt/open_sources/scripts/tardis_config.yaml"
-# export VLLM_USE_V1=1
-# export VLLM_ENABLE_V1_MULTIPROCESSING=1
-# export VLLM_WORKER_MULTIPROC_METHOD="spawn"
-# export VLLM_FLASH_ATTN_VERSION=3
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ["TARDIS_CONFIG_FILE"] = "/home/wxt/open_sources/scripts/tardis_config.yaml"
-# os.environ["LMCACHE_CONFIG_FILE"] = "/home/wxt/open_sources/scripts/lmcache_config.yaml"
-# os.environ["LMCACHE_USE_EXPERIMENTAL"] = "True"
+os.environ["LMCACHE_CONFIG_FILE"] = "/home/wxt/open_sources/scripts/lmcache_config.yaml"
+os.environ["LMCACHE_USE_EXPERIMENTAL"] = "True"
 os.environ["VLLM_USE_V1"] = "1"
 os.environ["VLLM_ENABLE_V1_MULTIPROCESSING"] = "1"
 os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
@@ -34,31 +28,54 @@ with open("/home/wxt/open_sources/vllm/examples/offline_inference/basic/man-bash
 # a truncation of the long context for the --max-model-len 16384
 # if you increase the --max-model-len, you can decrease the truncation i.e.
 # use more of the long context
-long_context = long_context[:70000]
+long_context = long_context[:155000]
 
 tokenizer = AutoTokenizer.from_pretrained("/home/wxt/models/Llama-3.1-8B-Instruct")
 question = "Summarize bash in 2 sentences."
 
 prompt = f"{long_context}\n\n{question}"
+prompt1 = f"{long_context[1:]}\n\n{question}"
+prompt2 = f"{long_context[2:]}\n\n{question}"
+prompt3 = f"{long_context[3:]}\n\n{question}"
+
+prompt4 = f"{long_context[:75000]} {long_context[:75000]}\n\n{question}"
+
+prompt5 = f"{long_context[:75000]} {long_context[1:75000]}\n\n{question}"
+prompt6 = f"{long_context[:75000]} {long_context[2:75000]}\n\n{question}"
+
+print(f"Prompt length: {len(tokenizer.encode(prompt))} tokens")
 
 # Sample prompts.
 prompts = [
-    # "Hello, my name is",
-    # "The president of the United States is",
-    # "The capital of France is",
-    # "The future of AI is",
-    prompt
+    prompt,
+    prompt,
+    prompt,
+    prompt3,
+    # prompt4,
+    # prompt5,
+    # prompt6,
 ]
 # Create a sampling params object.
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+sampling_params = SamplingParams(
+    temperature=0.8, top_p=0.95,
+    # max_tokens=1024,
+    # ignore_eos=True,
+    # stop=None 
+    )
 
 
 def main():
     # Create an LLM.
     llm = LLM(model="/home/wxt/models/Llama-3.1-8B-Instruct",
-            enforce_eager=True, block_size=256, enable_prefix_caching=False,
-            kv_transfer_config=ktc,
-            max_num_batched_tokens=65536, max_model_len=131072)
+            enforce_eager=True, enable_prefix_caching=False,
+            kv_transfer_config=ktc, block_size=256,
+            max_num_batched_tokens=131072, max_model_len=131072)
+
+    # llm = LLM(model="/home/wxt/models/Llama-3.1-8B-Instruct",
+    #         enforce_eager=True, 
+    #         # tensor_parallel_size=2,
+    #         max_num_batched_tokens=131072, max_model_len=131072)
+    
     # Generate texts from the prompts.
     # The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
@@ -71,13 +88,13 @@ def main():
         print(f"Output:    {generated_text!r}")
         print("-" * 60)
     
-    outputs = llm.generate(prompt, sampling_params)
-    # Print the outputs.
-    print("\nGenerated Outputs:\n" + "-" * 60)
-    for output in outputs:
-        generated_text = output.outputs[0].text
-        print(f"Output:    {generated_text!r}")
-        print("-" * 60)
+    # outputs = llm.generate(prompts, sampling_params)
+    # # Print the outputs.
+    # print("\nGenerated Outputs:\n" + "-" * 60)
+    # for output in outputs:
+    #     generated_text = output.outputs[0].text
+    #     print(f"Output:    {generated_text!r}")
+    #     print("-" * 60)
     torch.cuda.cudart().cudaProfilerStop()
 
 
--- examples/offline_inference/basic/profile.sh
+++ examples/offline_inference/basic/profile.sh
@@ -1,6 +1,17 @@
+
+# sudo -E /home/wxt/miniconda3/envs/wxttest/bin/python basic.py
+
 sudo -E \
 /usr/local/cuda/bin/nsys profile \
     --trace-fork-before-exec=true \
     --gpu-metrics-devices=0 \
     --capture-range cudaProfilerApi \
 /home/wxt/miniconda3/envs/wxttest/bin/python basic.py
+
+
+# sudo -E \
+# /usr/local/cuda/bin/ncu \
+#     --set full \
+#     --target-processes all \
+#     --kernel-id :13:nvme_batch_xfer_kernel: \
+# /home/wxt/miniconda3/envs/wxttest/bin/python basic.py
